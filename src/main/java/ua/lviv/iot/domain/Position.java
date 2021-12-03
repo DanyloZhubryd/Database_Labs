8a36e6a6b8d63c36efa45d64a35ecf933c020b84
Zhubryd Danylo <zubdan21@gmail.com>
2021-12-03 22:37:04 +0200

    Domains and Repositories

---
+++ src/main/java/ua/lviv/iot/domain/Position.java
@@ -0,0 +1,45 @@
+package ua.lviv.iot.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "position")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Position {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "salary", nullable = false)
+    private Float salary;
+
+    //refernces
+
+    @OneToMany(mappedBy = "position", fetch = FetchType.LAZY)
+    private Collection<Worker> workers;
+
+    @ManyToMany(mappedBy = "positions")
+    private Set<Kindergarden> kindergardens;
+
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
